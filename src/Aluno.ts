ee8ace678fa4c1313549af3c124a4c7cfb755fbc
Thabata Moraes <90910437+Thabata-Moraes@users.noreply.github.com>
2023-02-16 12:13:58 +0000

    exercicio 2

--- src/Aluno.ts
+++ src/Aluno.ts
@@ -4,9 +4,11 @@ class Aluno{
     idade: number; 
     p1: number;
     p2: number;
-    media: number
+    media: number;
+    resultado: string;
     notaFinal(){
         this.media = (this.p1 + this.p2)/2
+        console.log(this.media)
     }
     dadosAluno(){
         return (
@@ -18,10 +20,22 @@ class Aluno{
             
         )
     }
+    passou(){
+        this.notaFinal();
+        this.resultado = this.media > 6? 'Aprovado' : 'Reprovado'
+        console.log(this.resultado)
+    }
 }
+
+
 
 let aluno1 = new Aluno()
 aluno1.nome = 'Jose'
 aluno1.numeroAluno = 123
 aluno1.idade = 30
+aluno1.p1 = 6
+aluno1.p2 = 8
+console.log(aluno1.passou())
+aluno1.notaFinal()
 aluno1.dadosAluno()
+
