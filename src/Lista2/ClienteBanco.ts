f2cff3701baca0f11db542d9b753e68a50f08290
Thabata Moraes <90910437+Thabata-Moraes@users.noreply.github.com>
2023-03-07 12:17:03 +0000

    teste

--- src/Lista2/ClienteBanco.ts
+++ src/Lista2/ClienteBanco.ts
@@ -1,8 +1,8 @@
 class ClienteBanco{
-    public numeroConta: string;
-    public numeroAgencia: string;
-    public nome: string;
-    public saldo: number;
+    private numeroConta: string;
+    private numeroAgencia: string;
+    private nome: string;
+    private saldo: number;
 
     constructor(numeroConta: string, numeroAgencia: string, nome: string, saldo: number){
         this.setNumeroConta(numeroConta)
@@ -19,6 +19,7 @@ class ClienteBanco{
     dadosCliente():string{
         return(
                 `
+                Número Agência: ${this.numeroAgencia}
                 Número Conta: ${this.numeroConta}
                 Nome: ${this.nome}
                 Saldo: ${this.saldo}
@@ -26,17 +27,27 @@ class ClienteBanco{
         )
     }
     setNumeroConta(numeroConta:string): void{
-        if (numeroConta.length == 8 && numeroConta.charAt(6) == '-'){
+        if (numeroConta.length == 7 && numeroConta.charAt(5) == '-'){
             this.numeroConta = numeroConta
         } else {
+            this.numeroConta = '00000-00'
             console.log ('Dados incorretos')
         }
     }
     setNumeroAgencia(numeroAgencia:string): void{
-        this.numeroAgencia = numeroAgencia
+        if ((numeroAgencia.length == 5) && (numeroAgencia.charAt(3) == '-')){
+            this.numeroAgencia = numeroAgencia
+        } else {
+            this.numeroAgencia = '00000-00'
+            console.log ('Dados incorretos')
+        }
     }
     setNome(nome:string):void{
-        if 
+        if (nome.length <= 30){
+            this.nome = nome
+        } else {
+            console.log('O nome digitado estrapola os carecteres')
+        }
     }
     setSaldo(saldo:number): void{
         if (saldo >= 0){
@@ -49,16 +60,25 @@ class ClienteBanco{
     getSaldo(): number{
         return this.saldo
     }
+    getNumeroAgencia(): string{
+        return this.numeroAgencia
+    }
+    getNumeroConta(): string{
+        return this.numeroConta
+    }
+    getNome(): string{
+        return this.nome
+    }
 }
 
-let ClienteBanco1 = new ClienteBanco('12345-9', '80','Tonho', 1200)
+let ClienteBanco1 = new ClienteBanco('12345-98', '234-65','Tonho', 1200)
 
 //let ClienteBanco2 = new ClienteBanco(123, 28, 'Creiton', 10000)
 
 console.log(ClienteBanco1.dadosCliente())
-ClienteBanco1.realizarDeposito(80)
-ClienteBanco1.realizarSaque(20)
-console.log(ClienteBanco1.dadosCliente())
+//ClienteBanco1.realizarDeposito(80)
+//ClienteBanco1.realizarSaque(20)
+//console.log(ClienteBanco1.dadosCliente())
 
 //console.log(ClienteBanco2.dadosCliente())
 //ClienteBanco2.realizarDeposito(2000)
