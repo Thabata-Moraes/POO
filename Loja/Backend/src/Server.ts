ddf548c07a5afafe2165ab05dbd932ac22e39c0c
Thabata Moraes <90910437+Thabata-Moraes@users.noreply.github.com>
2023-04-18 10:55:16 -0300

    back e prisma rodando

--- Loja/Backend/src/Server.ts
+++ Loja/Backend/src/Server.ts
@@ -1,10 +1,32 @@
 // importa a classe fastify da dependência fastify
 import Fastify from "fastify";
+import {PrismaClient} from '@prisma/client'
+
 // instanciar o obj da classe fastify 
 const app = Fastify()
+
+// Instanciar obj da classe PrismaClient
+const prisma = new PrismaClient()
+
 // definir uma rota chamada hello- verbo é get, uma consulta
 app.get('/hello', () => {
     return 'Hello World ihuul'
+})
+
+app.get('/products',async () => {
+    const habbits = await prisma.product.findMany()
+    return habbits
+})
+
+app.get('/productByName',async () => {
+    const habbits = await prisma.product.findMany({
+        where: {
+            name: {
+                startsWith: 'X'
+            }
+        }
+    })
+    return habbits
 })
 
 // vamos subir o servidor, vamos executá-lo, ele ficará ouvindo e aguardando as requisições 
