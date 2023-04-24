53bd79a01d1bc0ba45acf9c7c27d29eb18699b0b
Thabata Moraes <90910437+Thabata-Moraes@users.noreply.github.com>
2023-04-24 10:49:37 -0300

    aula 24/04 rotas

--- Loja/Backend/src/Server.ts
+++ Loja/Backend/src/Server.ts
@@ -1,6 +1,8 @@
 // importa a classe fastify da dependência fastify
 import Fastify from "fastify";
 import {PrismaClient} from '@prisma/client'
+import {z} from 'zod';
+import dayjs from "dayjs";
 
 // instanciar o obj da classe fastify 
 const app = Fastify()
@@ -13,20 +15,67 @@ app.get('/hello', () => {
     return 'Hello World ihuul'
 })
 
+//rota que consulta todos os produtos
 app.get('/products',async () => {
     const habbits = await prisma.product.findMany()
     return habbits
 })
 
-app.get('/productByName',async () => {
+//rota que consulta produtos específicos
+app.get('/productByName/:name',async (request) => {
+    const nameParam = z.object({
+        name: z.string()
+    })
+    const {name} = nameParam.parse(request.params)
     const habbits = await prisma.product.findMany({
+        //recupera o name informado pelo front
         where: {
             name: {
-                startsWith: 'X'
+                startsWith: name
             }
         }
     })
     return habbits
+})
+
+// define uma rota que cria um produto no banco de dados, usadndo o verbo post
+app.post('/product', async (request) => {
+    // recupera os dados do body (corpo da requisição)
+    const createProductBody = z.object({
+        name: z.string(),
+        description: z.string(),
+        quantity: z.number()
+    })
+    const {name, description, quantity} = createProductBody.parse(request.body)
+
+    //recupera a data atual:
+    const today = dayjs().startOf('day').toDate() //startOf só cria o dia, sem hora, min e segundo.
+
+    // Insere o produto no banco:
+    await prisma.product.create({
+        data : {
+            name,
+            description,
+            quantity,
+            created_at: today,
+        }
+    })   
+}) 
+
+//rota para remover um produto, usando o verbo delete
+app.delete('/product/:id', async (request) => {
+    //recupera o id para remoção
+    const idParam = z.object({
+        id: z.string().uuid()
+    })
+    const {id} = idParam.parse(request.params)
+    // remove o produto
+    await prisma.product.delete({
+        where: {
+            id: id
+        }
+    })
+    
 })
 
 // vamos subir o servidor, vamos executá-lo, ele ficará ouvindo e aguardando as requisições 
