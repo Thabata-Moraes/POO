d470e39ec9fa05efae6fcb4d9192d8fd9e30b006
Thabata Moraes <90910437+Thabata-Moraes@users.noreply.github.com>
2023-05-09 10:57:10 -0300

    (09/05)

--- Loja/Backend/src/Server.ts
+++ Loja/Backend/src/Server.ts
@@ -1,12 +1,16 @@
 // importa a classe fastify da dependência fastify
 import Fastify from "fastify";
 import { AppRoutes } from "./routes";
+import cors from '@fastify/cors'
 
 // instanciar o obj da classe fastify 
 const app = Fastify()
 
 // Registro todas as rotas no servidor HTTP
 app.register(AppRoutes)
+
+// Dependencia cors que possibilita o compartilhamento de informação entre API e diferentes IPs 
+app.register(cors)
 
 // vamos subir o servidor, vamos executá-lo, ele ficará ouvindo e aguardando as requisições 
 app.listen({
--- Loja/Frontend/scripts.js
+++ Loja/Frontend/scripts.js
@@ -1,7 +1,33 @@
+
 async function query(){
     let products = await fetch('http://localhost:3333/products')
     .then(response => {
         return response.json()
     })
-    console.log(products)
+    let content =''
+    products.forEach(product => {
+        content += `<tr> <td> ${product.name} </td> <td> ${product.description} </td> <td> ${product.quantity} </td> <td> ${product.created_at} </td> </tr>`
+    });
+    document.getElementById("table").innerHTML = content
+
+}   
+
+async function register(){
+    let name = document.getElementById("name").value
+    let description = document.getElementById("description").value
+    let quantity = Number(document.getElementById("quantity").value)
+    let product = {name, description, quantity}
+    await fetch('http://localhost:3333/product', {
+        method: 'POST',
+        body: JSON.stringify(product), //converte obj JSON em string
+        headers: {
+            "Content-Type" : "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => alert('Inserção realizada com sucesso'))
+    .catch(error => alert('Problema na inserção'))
+    // chama a função consulta
+    query()
+
+
 }   
